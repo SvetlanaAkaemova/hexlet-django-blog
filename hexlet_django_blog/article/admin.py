80e3bf9f4b66900c5442081c4324a29753a7854b
svetlana_akaemova <akaemova.sv@mail.ru>
2023-02-12 16:44:51 +0100

    add admin

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+
+from .models import Article
 
 # Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp') # Перечисляем поля, отображаемые в таблице списка статей
+    search_fields = ['name', 'body']
+    list_filter = (('timestamp', DateFieldListFilter),) # Перечисляем поля для фильтрации
